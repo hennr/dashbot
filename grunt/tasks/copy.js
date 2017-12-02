d4129271741ca6a36918b2cd6ea1dc86cf22c24b
Jan-Hendrik Peters <hennr@hennr.name>
2017-12-02 22:06:32 +0100

    drop css min

--- grunt/tasks/copy.js
+++ grunt/tasks/copy.js
@@ -1,32 +1,38 @@
 module.exports = {
-  angular: {
-    cwd: '<%= paths.src.angular %>',
-    src: 'angular.min.js',
-    dest: '<%= paths.dist.angular %>',
-    expand: true
-  },
-  fonts: {
-    cwd: '<%= paths.src.fonts %>',
-    src: '**/*',
-    dest: '<%= paths.dist.fonts %>',
-    expand: true
-  },
-  images: {
-    cwd: '<%= paths.src.images %>',
-    src: '**/*',
-    dest: '<%= paths.dist.images %>',
-    expand: true
-  },
-  templates: {
-    cwd: '<%= paths.src.templates %>',
-    src: '**/*',
-    dest: '<%= paths.dist.templates %>',
-    expand: true
-  },
-  data: {
-    cwd: '<%= paths.src.data %>',
-    src: '**/*.json',
-    dest: '<%= paths.dist.data %>',
-    expand: true
-  }
+    angular: {
+        cwd: '<%= paths.src.angular %>',
+        src: 'angular.min.js',
+        dest: '<%= paths.dist.angular %>',
+        expand: true
+    },
+    fonts: {
+        cwd: '<%= paths.src.fonts %>',
+        src: '**/*',
+        dest: '<%= paths.dist.fonts %>',
+        expand: true
+    },
+    images: {
+        cwd: '<%= paths.src.images %>',
+        src: '**/*',
+        dest: '<%= paths.dist.images %>',
+        expand: true
+    },
+    css: {
+        cwd: '<%= paths.src.css %>',
+        src: '**/app.css',
+        dest: '<%= paths.dist.css %>',
+        expand: true
+    },
+    templates: {
+        cwd: '<%= paths.src.templates %>',
+        src: '**/*',
+        dest: '<%= paths.dist.templates %>',
+        expand: true
+    },
+    data: {
+        cwd: '<%= paths.src.data %>',
+        src: '**/*.json',
+        dest: '<%= paths.dist.data %>',
+        expand: true
+    }
 };
--- grunt/tasks/cssmin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  all: {
-    files: {
-      '<%= paths.dist.css %>/dashbot.min.css': [
-        '<%= paths.src.css %>/**/*.css'
-      ]
-    }
-  }
-};
